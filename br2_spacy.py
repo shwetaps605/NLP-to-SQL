ebcc0cc59e73604c2d8c2cca895d0451a162c8ca
shwetaps605 <shwetaroy305@gmail.com>
2020-05-01 01:03:50 +0530

    Added test cases if no query generated

--- br2_spacy.py
+++ br2_spacy.py
@@ -122,19 +122,24 @@ def get_pos(query):
                 a=list(set(a).intersection(attr_list))
             print(f"{i} : reduced to {a}\n")
 
-    print(f"The possible attributes are:\n{a}\n")
-    
-    sim_dict={}
-    s=0
-    for i in a:
+    if(len(a)!=0):
+        print(f"The possible attributes are:\n{a}\n")
+        
+        sim_dict={}
         s=0
-        for j in list_of_nouns :
-            s+=fuzz.token_set_ratio(i,j)
-        sim_dict[i]=s
-    print(sim_dict)
-    
-    a_list = max(sim_dict, key=sim_dict.get).split() 
-    a_max= ",".join(map(str,a_list))
+        for i in a:
+            s=0
+            for j in list_of_nouns :
+                s+=fuzz.token_set_ratio(i,j)
+            sim_dict[i]=s
+        print(sim_dict)
+        
+        a_list = max(sim_dict, key=sim_dict.get).split() 
+        a_max= ",".join(map(str,a_list))
+
+    else:
+        a_max=""
+
     return a_max
 
 sum_synonyms=['sum','total']
@@ -203,19 +208,22 @@ def get_attributes(clause):
     attribute_string=[]
     tables_string=""
     order_by_string=""
+    group_by_string=""
     o_str=""
     a_str=""
     attr=""
     
     que_list=re.split('and|,',clause)
-    group_by_clause=re.split("each"," ".join(map(str,que_list)))
-    group_by_attribute=get_pos(group_by_clause[1])
-    group_by_string=f"GROUP BY {group_by_attribute}"
-    print("EACH KA SPLIT\n",group_by_clause)
-    print("EACH KA ATTRIBUTE\n",group_by_attribute)
     print(que_list)
     
     for i in que_list:
+
+        if ("each" in i.lower().split()):
+            group_by_clause=re.split("each"," ".join(map(str,que_list)))
+            group_by_attribute=get_pos(group_by_clause[1])
+            group_by_string=f"GROUP BY {group_by_attribute}"
+            print("EACH KA SPLIT\n",group_by_clause)
+            print("EACH KA ATTRIBUTE\n",group_by_attribute)
 
         if(checkAggregateSum(i)):
             print("SUM hai\n",i)
@@ -254,33 +262,36 @@ def get_attributes(clause):
 
         print("###",i)
         a=get_pos(i)
-        print(f"\nATTRIBUTE FOUND:{a}\n")
 
-        if a not in attributes_list:
-            attributes_list.append(a)
-            
-        tab=list(data[data['Attributes']==a]['Table Name'])
-        tables=",".join(map(str,tab))
-        if tables not in tab_list:
-            tab_list.append(tables)
+        if(len(a)!=0):
+            print(f"\nATTRIBUTE FOUND:{a}\n")
 
-        if(len(a_str)!=0):
-            attr=a_str+"("+str(a)+")"
-            
+            if a not in attributes_list:
+                attributes_list.append(a)
+                
+            tab=list(data[data['Attributes']==a]['Table Name'])
+            tables=",".join(map(str,tab))
+            if tables not in tab_list:
+                tab_list.append(tables)
+
+            if(len(a_str)!=0):
+                attr=a_str+"("+str(a)+")"
+                
+            else:
+                attr=str(a)
+
+            attribute_string.append(attr)
+
+            if(len(o_str)!=0):
+                order_by_string+=str(a)+" "+o_str
+
+            print("HMMMM\n",attribute_string)
+            print(f"The corresponding attributes are:{attributes_list}")
+            print(f"The corresponding tables are:{tab_list}\n")
+
         else:
-            attr=str(a)
+            attribute_string=""
 
-        attribute_string.append(attr)
-
-        if(len(o_str)!=0):
-            order_by_string+=str(a)+" "+o_str
-
-
-        print("HMMMM\n",attribute_string)
-
-    
-    print(f"The corresponding attributes are:{attributes_list}")
-    print(f"The corresponding tables are:{tab_list}\n")
             
     return attribute_string,tab_list,order_by_string,group_by_string
 
@@ -320,7 +331,7 @@ def get_pk_fk(tab_list):
             r1=PK_FK[(PK_FK['Table1']== li[1]) & (PK_FK['Table2']== li[0])]
 
             if(r.shape[0]!=0):
-               
+                print("PEHLE WALE ME HU\n")
                 for k1 in list(r['Key-Table1']):
                     if k1 not in key_table1_list:
                         key_table1_list.append(k1)
@@ -333,7 +344,7 @@ def get_pk_fk(tab_list):
 
 
             elif(r1.shape[0]!=0):
-                
+                print("DUSRE WALE ME HU\n")
                 for k1 in list(r1['Key-Table2']):
                     if k1 not in key_table1_list:
                         key_table1_list.append(k1)
@@ -356,7 +367,7 @@ def get_pk_fk(tab_list):
         
     return where_string
 
-equal_synonyms=['equal', '=','==']
+equal_synonyms=['equal','=','==']
 greater_synonyms=['greater','larger','bigger','>']
 lesser_synonyms=['lesser','smaller','less','<']
 
@@ -364,9 +375,8 @@ lesser_synonyms=['lesser','smaller','less','<']
 def checkEqual(q):
     print("EQUAL HAI\n")
     li=q.lower().split()
-    match=(set(equal_synonyms).intersection(li))
-    if(len(list(match)>0)):
-        return match
+    if(len(list(set(equal_synonyms).intersection(li)))>0):
+        return True
     else:
         return False
     
@@ -397,16 +407,20 @@ def get_conditional_attributes2(clause):
         print("Conditional Clause:\n",i)
         
         if(checkEqual(i) and checkGreater(i)):
+            print("11\n")
             val_list=re.split('\sequal to\s|=|==',i)
             val=val_list[1:]
             sign=">="
         
         elif(checkEqual(i) and checkLesser(i)):
+            print("22\n")
             val_list=re.split('\sequal to\s|=|==',i)
             val=val_list[1:]
             sign="<="
         
         elif(checkEqual(i)):
+            print("33\n")
+            print("sdjhfsdf\n")
             val_list=re.split('\sequal to\s|=|==',i)
             val=val_list[1:]
             sign="="
@@ -414,16 +428,19 @@ def get_conditional_attributes2(clause):
             print(val[0])
             
         elif (checkGreater(i)):
+            print("44\n")
             val_list=re.split('greater than|larger than|bigger than|>',i)
             val=val_list[1:]
             sign=">"
             
         elif(checkLesser(i)):
+            print("55\n")
             val_list=re.split('less than|smaller than|<',i)
             val=val_list[1:]
             sign="<"
             
         elif("between" in i.lower().split() or "range" in i.lower().split() ):
+            print("66\n")
             val_list=re.split('\sbetween\s|\srange\s',i)
             bet_val_list=re.split('-|to',val_list[1].strip())
             bet_val_first=bet_val_list[0].strip()
@@ -466,26 +483,34 @@ def convert_into_sql(query):
         conditional_query=""
         t=t1
 
-    attr_string=",".join(map(str,a1))
-    tab_string=",".join(map(str,t))
-    pk_fk_part=get_pk_fk(t)
-   
-    if(len(pk_fk_part)==0 and len(conditional_query)<=1 ):
-        where_string=""
-        sql_query=f"SELECT DISTINCT {attr_string} \nFROM {tab_string}"
-    elif(len(pk_fk_part)>0 and len(conditional_query)<=1):
-        where_string=pk_fk_part
-        sql_query=f"SELECT DISTINCT {attr_string} \nFROM {tab_string} \nWHERE {where_string}"
-    else:
-        where_string=pk_fk_part+" AND "+conditional_query
-        sql_query=f"SELECT DISTINCT {attr_string} \nFROM {tab_string} \nWHERE {where_string}"
-
-    if(len(order_by)!=0):
-        sql_query+="\nORDER BY"+" "+order_by
-
-    if(len(group_by)!=0):
-        sql_query+="\n"+group_by
     
+    if (len(a1)!=0):
+        attr_string=",".join(map(str,a1))
+        tab_string=",".join(map(str,t))
+        pk_fk_part=get_pk_fk(t)
+    
+        if(len(pk_fk_part)==0 and len(conditional_query)<=1 ):
+            where_string=""
+            sql_query=f"SELECT DISTINCT {attr_string} \nFROM {tab_string}"
+        elif(len(pk_fk_part)>0 and len(conditional_query)<=1):
+            where_string=pk_fk_part
+            sql_query=f"SELECT DISTINCT {attr_string} \nFROM {tab_string} \nWHERE {where_string}"
+        else:
+            where_string=pk_fk_part+" AND "+conditional_query
+            sql_query=f"SELECT DISTINCT {attr_string} \nFROM {tab_string} \nWHERE {where_string}"
+
+        if(len(order_by)!=0):
+            sql_query+="\nORDER BY"+" "+order_by
+
+        if(len(group_by)!=0):
+            sql_query+="\n"+group_by
+
+    else:
+        print("CONASNA\n")
+        sql_query=""
+
+    print("SQL JO BANI HAI:\n",sql_query)
+
     return sql_query
 
 
@@ -498,41 +523,54 @@ def generate_data(query):
     charset=charSet,
     cursorclass=cusrorType)
 
-    sql_query=convert_into_sql(query)
-    print(f"SQL generated:\n\n{sql_query}\n")
-
-    NEWLINE='\n'
+    sql_query=""
+    data_to_be_displayed=""
 
     try:
-        cur = connectionInstance.cursor()                       
-        cur.execute("USE INS")
+        sql_query=convert_into_sql(query)
+        if(len(sql_query)!=0):
+            print(f"SQL generated:\n\n{sql_query}\n")
+            NEWLINE='\n'
+            try:
+                cur = connectionInstance.cursor()                       
+                cur.execute("USE INS")
 
-        sqlQuery    = sql_query
-        cur.execute(sqlQuery)
-        rows = cur.fetchall()
-        data_to_be_displayed=""
-        
-        attribute_names= [r.keys() for r in rows]
-        header=attribute_names[0]
-        data_to_be_displayed+="\t".join(map(str,list(header)))+NEWLINE
+                sqlQuery    = sql_query
+                cur.execute(sqlQuery)
+                rows = cur.fetchall()
+                data_to_be_displayed=""
+                    
+                attribute_names= [r.keys() for r in rows]
+                header=attribute_names[0]
+                data_to_be_displayed+="\t".join(map(str,list(header)))+NEWLINE
 
-        for row in rows:
-            records="\t\t\t\t".join(map(str,list(row.values())))
-            data_to_be_displayed+=records+NEWLINE
-            
+                for row in rows:
+                    records="\t\t\t\t".join(map(str,list(row.values())))
+                    data_to_be_displayed+=records+NEWLINE
+                
+
+            except Exception as e:
+                print("Exeception occured:{}".format(e))
+                data_to_be_displayed="NO RESULTS FOUND"
+
+            finally:
+                connectionInstance.close()
+
+        else:
+            print("NO RESULTS FOUND\n")
+            data_to_be_displayed="NO RESULTS FOUND"
 
     except Exception as e:
         print("Exeception occured:{}".format(e))
-        data_to_be_displayed=" "
+        print("NO QUERY GENERATED\n")
+        data_to_be_displayed="NO RESULTS FOUND"
 
-    finally:
-        connectionInstance.close()
 
     return data_to_be_displayed
 
-nl_query="Display case approval date and beneficiary visas"
-ss= generate_data(nl_query.lower())
-print(ss)
+#nl_query="Display case approval date and petitioner names  where annual salary is equal to 50"
+#ss= generate_data(nl_query.lower())
+#print(ss)
 
 
 
